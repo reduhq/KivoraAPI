c5f999d4c24b51b343d2fff83813895bebffbc84
craftxdog <aaronmars77@gmail.com>
2024-08-12 22:46:34 -0600

    test(dev): installing morgan package and add in index file

--- src/Kivora/Settings.ts
+++ src/Kivora/Settings.ts
@@ -1,15 +1,17 @@
-import crypto from 'crypto'
+import "dotenv/config";
+import crypto from "crypto";
 
-class Settings{
-    public readonly SERVER_HOST:string = process.env.SERVER_HOST as string
-    public readonly PORT:number = parseInt(process.env.PORT ?? '8000')
-    public readonly API_V1_STR:string = '/api/v1'
+class Settings {
+  public readonly SERVER_HOST: string = process.env.SERVER_HOST as string;
+  public readonly PORT: number = parseInt(process.env.PORT ?? "8000");
+  public readonly API_V1_STR: string = "/api/v1";
 
-    public readonly SECRET_KEY:string = crypto.randomBytes(32).toString('hex')
-    public readonly ACCESS_TOKEN_EXPIRES_MINUTES:number = 60 * 5
-    public readonly ALGORITHM:string = "HS256"
+  public readonly SECRET_KEY: string = crypto.randomBytes(32).toString("hex");
+  public readonly ACCESS_TOKEN_EXPIRES_MINUTES: number = 60 * 5;
+  public readonly ALGORITHM: string = "HS256";
 }
 
-const settings = new Settings()
+const settings = new Settings();
 
-export default settings
+export default settings;
+
--- src/Kivora/index.ts
+++ src/Kivora/index.ts
@@ -1,25 +1,27 @@
 import "reflect-metadata";
-import express from 'express'
-import { InversifyExpressServer } from 'inversify-express-utils'
-import { container } from './inversify.config'
+import express from "express";
+import { InversifyExpressServer } from "inversify-express-utils";
+import { container } from "./inversify.config";
 import swaggerDocs from "./swagger";
 import settings from "./Settings";
+import morgan from "morgan";
 
+const PORT = settings.PORT;
 
-const PORT = settings.PORT
+const server = new InversifyExpressServer(container);
 
-const server = new InversifyExpressServer(container)
+server.setConfig((app) => {
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(express.urlencoded({ extended: true }));
+  swaggerDocs(app);
+});
 
-server.setConfig(app =>{
-    app.use(express.json())
-    app.use(express.urlencoded({extended: true}))
-    swaggerDocs(app)
-})
+import "./Controllers/v1/UserController";
+import "./Controllers/v1/LoginController";
 
-import './Controllers/v1/UserController'
-import './Controllers/v1/LoginController'
-const app = server.build()
+const app = server.build();
 
-app.listen(PORT, ()=>{
-    console.log(`🚀 Server running on ${settings.SERVER_HOST}/api`)
-})
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on ${settings.SERVER_HOST}/api`);
+});
